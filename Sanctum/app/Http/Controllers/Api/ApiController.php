cb7ab91b581491556852cabfe574b92c6fef3f42
MEHEDI HASAN <110008381+mehedimohammad213@users.noreply.github.com>
2025-06-27 11:58:58 +0600

    Initial commit

---
+++ Sanctum/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    // Register API - name, email, password, password_confirmation
+    public function register(Request $request){
+
+        $request->validate([
+            "name" => "required|string",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|confirmed",
+        ]);
+
+        User::create($request->all());
+
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully"
+        ]);
+    }
+
+    // Login API
+    public function login(Request $request){
+
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        // User check by Email
+        $user = User::where("email", $request->email)->first();
+
+        if(!empty($user)){
+
+            // Password check
+            if(Hash::check($request->password, $user->password)){
+
+                $token = $user->createToken("myToken")->plainTextToken;
+
+                return response()->json([
+                    "status" => true,
+                    "message" => "Logged in successfully",
+                    "token" => $token
+                ]);
+            } else {
+                return response()->json([
+                    "status" => false,
+                    "message" => "Password didn't match"
+                ]);
+            }
+        } else {
+            return response()->json([
+                "status" => false,
+                "message" => "Email is invalid"
+            ]);
+        }
+    }
+
+    // Profile API
+    public function profile(){
+
+        $userdata = auth()->user();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Profile data",
+            "data" => $userdata,
+            "id" => auth()->user()->id
+        ]);
+    }
+
+    // Logout API
+    public function logout(){
+
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            "status" => true,
+            "message" => "User logged out"
+        ]);
+    }
+}
